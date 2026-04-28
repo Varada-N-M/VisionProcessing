c8c6d748a4f2e027b2c96b8dd0990b5414539aee
Varada N M <89744897+Varada-N-M@users.noreply.github.com>
2026-04-28 09:37:16 +0530

    Change app port to use environment variable

--- app.py
+++ app.py
@@ -492,5 +492,5 @@ if __name__ == "__main__":
     _check("SPEECH_REGION",                _os.getenv("SPEECH_REGION", "eastus"), secret=False)
     print("───────────────────────────────────────────────\n")
     print("Press Ctrl+C to stop\n")
-
-    app.run(debug=True, host="localhost", port=3000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
